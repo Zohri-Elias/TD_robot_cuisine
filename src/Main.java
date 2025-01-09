c9c075a8f70bbe61c7d22def325ae169081a2ffe
Elias ZOHRI CHAKKOUF <ZOHRI-CHAKKOUF_El@lprs.local>
2025-01-09 16:33:22 +0100

    Let me cook

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Saisir le nom de l'ingredient");
+        String nom = sc.nextLine();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Saisir la quantite de l'ingredient");
+        double quantite = sc.nextDouble();
+        sc.skip("\n");
+
+        System.out.println("Saisir l'unite de l'ingredient");
+        String unite = sc.nextLine();
+        Ingredient MonIngredient = new Ingredient(nom, quantite, unite);
+        //Ingredient MonIngredient = new Ingredient("levure",1, "sachet");
+        System.out.println("La quantite de mon ingredient"+ MonIngredient.getQuantite());
+        System.out.println("La unite de mon ingredient"+ MonIngredient.getUnite());
+        System.out.println("La nom de mon ingredient"+ MonIngredient.getNom());
+        System.out.println(MonIngredient.afficher());
+        int nouvelleQuantite =25;
+        MonIngredient.setQuantite(nouvelleQuantite);
+        System.out.println(MonIngredient.afficher());
+
     }
 }
